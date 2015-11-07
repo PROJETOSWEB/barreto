0223dd1648901181662ab5c58b826e487e72f5ff
Thiago Maneschy <thiago@maneschy.com>
2015-11-07 16:10:26 -0400

    OLHA

---
+++ adm_investidor.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<!-- 
+Theme: JANGO - Ultimate Multipurpose HTML Theme Built With Twitter Bootstrap 3.3.5
+Version: 1.0.0
+Author: Themehats
+Site: http://www.themehats.com
+Contact: support@themehats.com
+Follow: www.twitter.com/themehats
+-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+<meta charset="utf-8"/>
+<title>BARRETO empreendimentos</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:100,300,600,900&amp;subset=all' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
+<link href="assets/plugins/socicon/socicon.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/bootstrap-social/bootstrap-social.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/animate/animate.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN: BASE PLUGINS  -->
+<link href="assets/plugins/revo-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/cubeportfolio/css/cubeportfolio.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/owl-carousel/owl.transitions.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/fancybox/jquery.fancybox.css" rel="stylesheet" type="text/css"/>
+<!-- END: BASE PLUGINS -->
+<!-- BEGIN THEME STYLES -->
+<link href="assets/base/css/plugins.css" rel="stylesheet" type="text/css"/>
+<link href="assets/base/css/components.css" id="style_components" rel="stylesheet" type="text/css"/>
+<link href="assets/base/css/themes/default.css" rel="stylesheet" id="style_theme" type="text/css"/>
+<link href="assets/base/css/custom.css" rel="stylesheet" type="text/css"/>
+<!-- END THEME STYLES -->
+<link rel="shortcut icon" href="assets/base/img/layout/logos/ico-barreto.png"/>
+</head>
+<body class="c-layout-header-fixed">
+<!-- BEGIN: LAYOUT/HEADERS/HEADER-1 -->
+<!-- BEGIN: HEADER -->
+<?php include './menu_barreto.php'; ?>
+<!-- END: HEADER -->
+<!-- END: LAYOUT/HEADERS/HEADER-1 -->
+
+<!-- BEGIN: PAGE CONTAINER --><div class="c-layout-page">	<!-- BEGIN: LAYOUT/BREADCRUMBS/BREADCRUMBS-1 -->	<div class="c-layout-breadcrumbs-1 c-fonts-uppercase c-fonts-bold">		<div class="container">			<div class="c-page-title c-pull-left">				<h3 class="c-font-uppercase c-font-bold" style="color: #BCBEC0;">Área restrita a investidores</h3>			</div>			<ul class="c-page-breadcrumbs c-theme-nav c-pull-right c-fonts-regular">				<li>				Olá Thiago Maneschy				</li>			</ul>		</div>	</div>	<!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-1 -->			<!-- BEGIN: PAGE CONTENT -->		<!-- BEGIN: CONTENT/CONTACT/FEEDBACK-1 -->	<div class="c-content-box c-size-md c-bg-white">		<div class="container">			<div class="c-content-title-1">				<h3 class="c-center c-font-dark c-font-uppercase">acompanhe seu investimento</h3>				<div class="c-line-center c-theme-bg">				</div>			</div>						<div class="c-content-panel">				<div class="c-body">					<div class="row">						<div class="col-md-4">						<h3><i class="fa fa-check-square"></i> INFORMAÇÕES</h3>							<ul class="c-content-list-1 c-theme c-separator-dot c-square">								<li>									Lorem ipsum dolor sit								</li>								<li>									Lorem ipsum dolor sit								</li>								<li>									Lorem ipsum dolor sit								</li>								<li>									Lorem ipsum dolor sit								</li>								<li>									Lorem ipsum dolor sit								</li>							</ul>						</div>												<div class="col-md-8">							<ul class="c-content-list-1 c-theme c-separator-dot c-square">								<img src="img/banner_adm.png" alt="" />							</ul>						</div>											</div>				</div>			</div>															<div class="c-content-panel">							<div class="c-body">				<h3><i class="fa fa-line-chart"></i> Andamento da obra</h3>					<div class="progress">						<div class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%"><strong style="font-size: 20px;">65%</strong>							<span class="sr-only">65% completos</span>						</div>					</div>				</div>												<div class="c-body">								<h3><i class="fa fa-folder-open"></i> Lista de documento para conferência</h3>				<br/>					<div class="row">						<div class="col-md-12">							<table class="table table-hover">							<thead>							<tr>								<th>									<i class="fa fa-file-pdf-o"></i>								</th>								<th>									DOCUMENTO								</th>								<th style="text-align: center;">									DATA								</th>								<th style="text-align: center;">									ASSUNTO								</th>								<th style="text-align: center;">									BAIXAR								</th>							</tr>							</thead>							<tbody>							<tr>								<th scope="row">									1								</th>								<td>									Planilha de custo								</td>								<td style="text-align: center;">									07-11-2015								</td>								<td style="text-align: center;">									Área de lazer								</td>								<td style="text-align: center;">									<a href="#"><span class="glyphicon glyphicon-cloud-download"> </span></a>								</td>							</tr>							<tr>								<th scope="row">									2								</th>								<td>									Planilha de custo								</td>								<td style="text-align: center;">									07-11-2015								</td>								<td style="text-align: center;">									Área de lazer								</td>								<td style="text-align: center;">									<a href="#"><span class="glyphicon glyphicon-cloud-download"> </span></a>								</td>							</tr>							<tr>								<th scope="row">									3								</th>								<td>									Planilha de custo								</td>								<td style="text-align: center;">									07-11-2015								</td>								<td style="text-align: center;">									Área de lazer								</td>								<td style="text-align: center;">									<a href="#"><span class="glyphicon glyphicon-cloud-download"> </span></a>								</td>							</tr>																					</tbody>							</table>						</div>					</div>				</div>			</div>					</div>						<div class="container">				<h1><i class="fa fa-photo"></i> ACOMPANHE TUDO POR FOTOS E VÍDEOS</h1>			<div id="filters-container" class="cbp-l-filters-button">							<div data-filter=".inicio" class="cbp-filter-item">					 INÍCIO DAS OBRAS					<div class="cbp-filter-counter">					</div>				</div>				<div data-filter=".construcoes" class="cbp-filter-item">					 CONSTRUÇÕES					<div class="cbp-filter-counter">					</div>				</div>				<div data-filter=".acabamentos" class="cbp-filter-item">					 ACABAMENTOS					<div class="cbp-filter-counter">					</div>				</div>				<div data-filter=".entrega" class="cbp-filter-item">					 ENTREGA					<div class="cbp-filter-counter">					</div>				</div>								<div data-filter=".videos" class="cbp-filter-item">					 VÍDEOS					<div class="cbp-filter-counter">					</div>				</div>											</div>			<div id="grid-container" class="cbp">							<div class="cbp-item construcoes">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<img src="assets/base/img/content/stock/60.jpg" alt="">						</div>						<div class="cbp-caption-activeWrap">							<div class="cbp-l-caption-alignCenter">								<div class="cbp-l-caption-body">									<a href="assets/base/img/content/stock/60.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn btn-sm c-btn-square c-btn-border-1x c-btn-white c-btn-uppercase" data-title="by Paul Flavius Nechita">ampliar</a>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						legenda da foto					</div>				</div>								<div class="cbp-item inicio">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<img src="assets/base/img/content/stock/57.jpg" alt="">						</div>						<div class="cbp-caption-activeWrap">							<div class="cbp-l-caption-alignCenter">								<div class="cbp-l-caption-body">									<a href="assets/base/img/content/stock/57.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn btn-sm c-btn-square c-btn-border-1x c-btn-white c-btn-uppercase" data-title="by Paul Flavius Nechita">ampliar</a>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						legenda da foto					</div>				</div>								<div class="cbp-item acabamento">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<img src="assets/base/img/content/stock/58.jpg" alt="">						</div>						<div class="cbp-caption-activeWrap">							<div class="cbp-l-caption-alignCenter">								<div class="cbp-l-caption-body">									<a href="assets/base/img/content/stock/58.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn btn-sm c-btn-square c-btn-border-1x c-btn-white c-btn-uppercase" data-title="by Paul Flavius Nechita">ampliar</a>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						legenda da foto					</div>				</div>								<div class="cbp-item construcoes">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<img src="assets/base/img/content/stock/54.jpg" alt="">						</div>						<div class="cbp-caption-activeWrap">							<div class="cbp-l-caption-alignCenter">								<div class="cbp-l-caption-body">									<a href="assets/base/img/content/stock/54.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn btn-sm c-btn-square c-btn-border-1x c-btn-white c-btn-uppercase" data-title="by Paul Flavius Nechita">ampliar</a>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						legenda da foto					</div>				</div>								<div class="cbp-item entrega">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<img src="assets/base/img/content/stock/59.jpg" alt="">						</div>						<div class="cbp-caption-activeWrap">							<div class="cbp-l-caption-alignCenter">								<div class="cbp-l-caption-body">									<a href="assets/base/img/content/stock/59.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn btn-sm c-btn-square c-btn-border-1x c-btn-white c-btn-uppercase" data-title="by Paul Flavius Nechita">ampliar</a>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						legenda da foto					</div>				</div>								<div class="cbp-item inicio">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<img src="assets/base/img/content/stock/55.jpg" alt="">						</div>						<div class="cbp-caption-activeWrap">							<div class="cbp-l-caption-alignCenter">								<div class="cbp-l-caption-body">									<a href="assets/base/img/content/stock/55.jpg" class="cbp-lightbox cbp-l-caption-buttonRight btn btn-sm c-btn-square c-btn-border-1x c-btn-white c-btn-uppercase" data-title="by Paul Flavius Nechita">ampliar</a>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						legenda da foto					</div>				</div>																<!-- VIDEOS SEMPRE POR ULTIMO -->				<div class="cbp-item videos">					<div class="cbp-caption">						<div class="cbp-caption-defaultWrap">							<div class="c-body">								<div class="embed-responsive embed-responsive-4by3">									<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/5pfaqk1WL3Y" frameborder="0" allowfullscreen>									</iframe>								</div>							</div>						</div>					</div>					<div class="cbp-l-grid-projects-desc">						LEGENDA DO VIDEO					</div>				</div>															</div>		</div>							</div>	<!-- END: CONTENT/CONTACT/FEEDBACK-1 -->			<!-- END: PAGE CONTENT --></div><!-- END: PAGE CONTAINER -->
+<!-- BEGIN: LAYOUT/FOOTERS/FOOTER-5 -->
+<a name="footer"></a>
+<?php include './footer_barreto.php'; ?>
+<!-- END: LAYOUT/FOOTERS/FOOTER-5 -->
+<!-- BEGIN: LAYOUT/FOOTERS/GO2TOP -->
+<div class="c-layout-go2top">
+	<i class="icon-arrow-up"></i>
+</div>
+<!-- END: LAYOUT/FOOTERS/GO2TOP -->
+<!-- BEGIN: LAYOUT/BASE/BOTTOM -->
+<!-- BEGIN: CORE PLUGINS -->
+<!--[if lt IE 9]>
+	<script src="../assets/global/plugins/excanvas.min.js"></script> 
+	<![endif]-->
+<script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- END: CORE PLUGINS -->
+<!-- BEGIN: LAYOUT PLUGINS -->
+<script src="assets/plugins/revo-slider/js/jquery.themepunch.tools.min.js" type="text/javascript"></script>
+<script src="assets/plugins/revo-slider/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+<script src="assets/plugins/cubeportfolio/js/jquery.cubeportfolio.min.js" type="text/javascript"></script>
+<script src="assets/plugins/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
+<script src="assets/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+<script src="assets/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+<script src="assets/plugins/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+<!-- END: LAYOUT PLUGINS -->
+<!-- BEGIN: THEME SCRIPTS --><script src="assets/base/js/scripts/pages/4col-portfolio.js" type="text/javascript"></script>
+<script src="assets/base/js/components.js" type="text/javascript"></script>
+<script src="assets/base/js/app.js" type="text/javascript"></script>
+<script>
+	$(document).ready(function() {    
+		App.init(); // init core    
+
+		//init main slider
+		var slider = $('.c-layout-revo-slider .tp-banner');
+	    var cont = $('.c-layout-revo-slider .tp-banner-container');
+	    var api = slider.show().revolution({
+	        delay: 15000,    
+	        startwidth:1170,
+	        startheight: (App.getViewPort().width < App.getBreakpoint('md') ? 1024 : 620),
+	        navigationType: "hide",
+	        navigationArrows: "solo",
+	        touchenabled: "on",
+	        onHoverStop: "on",
+	        keyboardNavigation: "off",
+	        navigationStyle: "circle",
+	        navigationHAlign: "center",
+	        navigationVAlign: "center",
+	        fullScreenAlignForce:"off",
+	        shadow: 0,
+	        fullWidth: "on",
+	        fullScreen: "off",       
+	        spinner: "spinner2",
+	        forceFullWidth: "on",
+	        hideTimerBar:"on",
+	        hideThumbsOnMobile: "on",
+	        hideNavDelayOnMobile: 1500,
+	        hideBulletsOnMobile: "on",
+	        hideArrowsOnMobile: "on",
+	        hideThumbsUnderResolution: 0,
+	        videoJsPath: "rs-plugin/videojs/",
+	    });
+	});
+	</script>
+<!-- END: THEME SCRIPTS -->
+<!-- END: LAYOUT/BASE/BOTTOM -->
+</body>
+</html>
