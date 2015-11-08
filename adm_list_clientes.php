6dddef14a4fd7a2436111cea09f03805a53588ca
Thiago Maneschy <thiago@MacBook-Pro-de-Thiago.local>
2015-11-08 19:13:33 -0400

    algumas

---
+++ adm_list_clientes.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<!-- 
+Theme: JANGO - Ultimate Multipurpose HTML Theme Built With Twitter Bootstrap 3.3.5
+Version: 1.0.0
+Author: Themehats
+Site: http://www.themehats.com
+Contact: support@themehats.com
+Follow: www.twitter.com/themehats
+-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+<meta charset="utf-8"/>
+<title>BARRETO empreendimentos</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:100,300,600,900&amp;subset=all' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
+<link href="assets/plugins/socicon/socicon.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/bootstrap-social/bootstrap-social.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/animate/animate.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN: BASE PLUGINS  -->
+<link href="assets/plugins/revo-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/cubeportfolio/css/cubeportfolio.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/owl-carousel/owl.theme.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/owl-carousel/owl.transitions.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/fancybox/jquery.fancybox.css" rel="stylesheet" type="text/css"/>
+<!-- END: BASE PLUGINS -->
+<!-- BEGIN THEME STYLES -->
+<link href="assets/base/css/plugins.css" rel="stylesheet" type="text/css"/>
+<link href="assets/base/css/components.css" id="style_components" rel="stylesheet" type="text/css"/>
+<link href="assets/base/css/themes/default.css" rel="stylesheet" id="style_theme" type="text/css"/>
+<link href="assets/base/css/custom.css" rel="stylesheet" type="text/css"/>
+<!-- END THEME STYLES -->
+<link rel="shortcut icon" href="assets/base/img/layout/logos/ico-barreto.png"/>
+</head>
+<body class="c-layout-header-fixed">
+<!-- BEGIN: LAYOUT/HEADERS/HEADER-1 -->
+<!-- BEGIN: HEADER -->
+<?php include './menu_barreto.php'; ?>
+<!-- END: HEADER -->
+<!-- END: LAYOUT/HEADERS/HEADER-1 -->
+
+<!-- BEGIN: PAGE CONTAINER --><div class="c-layout-page">	<!-- BEGIN: LAYOUT/BREADCRUMBS/BREADCRUMBS-1 -->	<div class="c-layout-breadcrumbs-1 c-fonts-uppercase c-fonts-bold">		<div class="container">			<div class="c-page-title c-pull-left">				<h3 class="c-font-uppercase c-font-bold" style="color: #BCBEC0;">Área administrtiva</h3>			</div>			<ul class="c-page-breadcrumbs c-theme-nav c-pull-right c-fonts-regular">				<li>				Olá Thiago Maneschy				</li>			</ul>		</div>	</div>	<!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-1 -->			<!-- BEGIN: PAGE CONTENT -->		<!-- BEGIN: CONTENT/CONTACT/FEEDBACK-1 -->	<div class="c-content-box c-size-md c-bg-white">		<div class="container">			<div class="c-content-title-1">				<div class="c-line-center c-theme-bg">				</div>			</div>															<div class="c-content-panel">			
+					<div class="c-body">
+						<p>
+							<a href="adm_list_empreendimentos.php" class="btn c-theme-btn c-btn-border-2x c-btn-square"><i class="glyphicon glyphicon-tower"></i> EMPREENDIMENTOS</a>
+							<a href="#" class="btn c-theme-btn"><i class="fa fa-group"></i> CLIENTES</a>
+							<a href="adm_banner.php" class="btn c-theme-btn c-btn-border-2x c-btn-square"><i class="fa fa-image"></i> BANNER</a>
+						</p>
+					</div>				<hr/>								<div class="c-body">
+				<a href="javascript:;" data-toggle="modal" data-target="#add_empreendimento"  class="btn btn-primary btn-xs" type="button"><i class="fa fa-group"></i></i> adicionar clientes</a>
+				<br/>				<br/>					<div class="row">						<div class="col-md-12">							<table class="table table-hover">							<thead>							<tr>								<th>								</th>								<th>									NOME DO CLIENTE								</th>
+								<th style="text-align: center;">
+									TELEFONE
+								</th>
+								<th style="text-align: center;">
+									EMPREENDIMENTO
+								</th>								<th style="text-align: center;">									DATA								</th>								<th style="text-align: center;">									EDITAR								</th>								<th style="text-align: center;">									EXCLUIR								</th>							</tr>							</thead>							<tbody>							<tr>								<th scope="row">									1								</th>								<td>									<a href="#">Fulano de tal</a>								</td>
+								<td style="text-align: center;">
+									(92) 98282-9292
+								</td>
+								<td style="text-align: center;">
+									Villa Floresta II
+								</td>
+								<td style="text-align: center;">
+									07-11-2015
+								</td>								<td style="text-align: center;">									<a href="javascript:;" data-toggle="modal" data-target="#edit_cliente"  ><i class="fa fa-pencil"></i></a>								</td>								<td style="text-align: center;">									<a href="#"><i class="fa fa-close"> </i></a>								</td>							</tr>							<tr>
+								<th scope="row">
+									2
+								</th>
+								<td>
+									<a href="#">Beltrano da Silva</a>
+								</td>
+								<td style="text-align: center;">
+									(92) 98282-9292
+								</td>
+								<td style="text-align: center;">
+									Villa Floresta I
+								</td>
+								<td style="text-align: center;">
+									07-11-2015
+								</td>
+								<td style="text-align: center;">
+									<a href="javascript:;" data-toggle="modal" data-target="#edit_cliente"  ><i class="fa fa-pencil"></i></a>
+								</td>
+								<td style="text-align: center;">
+									<a href="#"><i class="fa fa-close"> </i></a>
+								</td>
+							</tr>							<tr>
+								<th scope="row">
+									3
+								</th>
+								<td>
+									<a href="#">Cintia Almenida</a>
+								</td>
+								<td style="text-align: center;">
+									(92) 98282-9292
+								</td>
+								<td style="text-align: center;">
+									Villa Floresta III
+								</td>
+								<td style="text-align: center;">
+									07-11-2015
+								</td>
+								<td style="text-align: center;">
+									<a href="javascript:;" data-toggle="modal" data-target="#edit_cliente"  ><i class="fa fa-pencil"></i></a>
+								</td>
+								<td style="text-align: center;">
+									<a href="#"><i class="fa fa-close"> </i></a>
+								</td>
+							</tr>														<tr>
+								<th scope="row">
+									4
+								</th>
+								<td>
+									<a href="#">João Alberto</a>
+								</td>
+								<td style="text-align: center;">
+									(92) 98282-9292
+								</td>
+								<td style="text-align: center;">
+									Villa Floresta I
+								</td>
+								<td style="text-align: center;">
+									07-11-2015
+								</td>
+								<td style="text-align: center;">
+									<a href="javascript:;" data-toggle="modal" data-target="#edit_cliente"  ><i class="fa fa-pencil"></i></a>
+								</td>
+								<td style="text-align: center;">
+									<a href="#"><i class="fa fa-close"> </i></a>
+								</td>
+							</tr>																					</tbody>							</table>						</div>					</div>				</div>			</div>					</div>													</div>	<!-- END: CONTENT/CONTACT/FEEDBACK-1 -->
+	
+				<!-- END: PAGE CONTENT --></div><!-- END: PAGE CONTAINER -->
+<!-- BEGIN: LAYOUT/FOOTERS/FOOTER-5 -->
+<a name="footer"></a>
+<?php include './footer_barreto.php'; ?>
+<!-- END: LAYOUT/FOOTERS/FOOTER-5 -->
+<!-- BEGIN: LAYOUT/FOOTERS/GO2TOP -->
+<div class="c-layout-go2top">
+	<i class="icon-arrow-up"></i>
+</div>
+<!-- END: LAYOUT/FOOTERS/GO2TOP -->
+<!-- BEGIN: LAYOUT/BASE/BOTTOM -->
+<!-- BEGIN: CORE PLUGINS -->
+<!--[if lt IE 9]>
+	<script src="../assets/global/plugins/excanvas.min.js"></script> 
+	<![endif]-->
+<script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- END: CORE PLUGINS -->
+<!-- BEGIN: LAYOUT PLUGINS -->
+<script src="assets/plugins/revo-slider/js/jquery.themepunch.tools.min.js" type="text/javascript"></script>
+<script src="assets/plugins/revo-slider/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+<script src="assets/plugins/cubeportfolio/js/jquery.cubeportfolio.min.js" type="text/javascript"></script>
+<script src="assets/plugins/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
+<script src="assets/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+<script src="assets/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+<script src="assets/plugins/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+<!-- END: LAYOUT PLUGINS -->
+<!-- BEGIN: THEME SCRIPTS --><script src="assets/base/js/scripts/pages/4col-portfolio.js" type="text/javascript"></script>
+<script src="assets/base/js/components.js" type="text/javascript"></script>
+<script src="assets/base/js/app.js" type="text/javascript"></script>
+<script>
+	$(document).ready(function() {    
+		App.init(); // init core    
+
+		//init main slider
+		var slider = $('.c-layout-revo-slider .tp-banner');
+	    var cont = $('.c-layout-revo-slider .tp-banner-container');
+	    var api = slider.show().revolution({
+	        delay: 15000,    
+	        startwidth:1170,
+	        startheight: (App.getViewPort().width < App.getBreakpoint('md') ? 1024 : 620),
+	        navigationType: "hide",
+	        navigationArrows: "solo",
+	        touchenabled: "on",
+	        onHoverStop: "on",
+	        keyboardNavigation: "off",
+	        navigationStyle: "circle",
+	        navigationHAlign: "center",
+	        navigationVAlign: "center",
+	        fullScreenAlignForce:"off",
+	        shadow: 0,
+	        fullWidth: "on",
+	        fullScreen: "off",       
+	        spinner: "spinner2",
+	        forceFullWidth: "on",
+	        hideTimerBar:"on",
+	        hideThumbsOnMobile: "on",
+	        hideNavDelayOnMobile: 1500,
+	        hideBulletsOnMobile: "on",
+	        hideArrowsOnMobile: "on",
+	        hideThumbsUnderResolution: 0,
+	        videoJsPath: "rs-plugin/videojs/",
+	    });
+	});
+	</script>
+<!-- END: THEME SCRIPTS -->
+<!-- END: LAYOUT/BASE/BOTTOM -->
+</body>
+</html>
+
+
+<!-- FORMULARIO ADICIONAR EMPREENDIMENTO -->
+<div class="modal fade c-content-login-form" id="add_empreendimento" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content c-square">
+			<div class="modal-header c-no-border">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
+			<div class="modal-body">
+				<h3 class="c-font-24  c-font-bold c-font-blue">ADICIONAR CLIENTES</h3>
+				<p>
+					Por favor, preencha corretamente os dados para incluir um cliente ao sistema
+				</p>
+				<form>
+					<div class="form-group">
+						<input type="text" class="form-control input-lg c-square" id="signup-email" placeholder="Nome do cliente">
+					</div>
+					<div class="form-group">
+						<input type="email" class="form-control input-lg c-square" id="signup-username" placeholder="email-do cliente">
+					</div>
+					<div class="form-group">
+						<input type="email" class="form-control input-lg c-square" id="signup-fullname" placeholder="telefone">
+					</div>
+					<div class="form-group">
+						<input type="email" class="form-control input-lg c-square" id="signup-fullname" placeholder="numero do contrato">
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control input-lg c-square" id="signup-fullname" placeholder="escolha uma senha para o cliente">
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control input-lg c-square" id="signup-fullname" placeholder="confirme a senha">
+					</div>
+					<div class="form-group">
+						<select class="form-control input-lg c-square" id="signup-country">
+							<option value="1">ESCOLHA O EMPREENDIMENTO</option>
+							<option value="1">• • • • • • • • • • • • •</option>
+							<option value="1">Villa Floresta I</option>
+							<option value="1">Villa Floresta II</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn c-theme-btn btn-md c-btn-uppercase c-btn-bold c-btn-square c-btn-login">cadastrar</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- // FORMULARIO ADICIONAR EMPREENDIMENTO -->
+
