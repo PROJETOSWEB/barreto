ae147a35a6f5c748de7a4a4512e0b9120751288c
Thiago Maneschy <thiago@maneschy.com>
2016-01-19 22:54:00 -0400

    vai

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<!-- 
-Theme: JANGO - Ultimate Multipurpose HTML Theme Built With Twitter Bootstrap 3.3.5
-Version: 1.0.0
-Author: Themehats
-Site: http://www.themehats.com
-Contact: support@themehats.com
-Follow: www.twitter.com/themehats
--->
-<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
@@ -91,7 +82,7 @@ Follow: www.twitter.com/themehats
 						<a href="preco_custo.php" class="c-action-btn btn btn-lg c-btn-square c-theme-btn c-btn-bold c-btn-uppercase">conheça mais</a>
 					</div>
 				</li>
-				<!--fim -->								<!--fim --><!--BEGIN: SLIDE #2 -->				<li data-transition="fade" data-slotamount="1" data-masterspeed="1000">					<img alt="" src="assets/base/img/layout/slides/pessoas2.jpg" 						data-bgposition="center center" 						data-bgfit="cover" 						data-bgrepeat="no-repeat">					<div class="caption customin customout tp-resizeme" 						data-x="center" 						data-y="center" 						data-hoffset="" 						data-voffset="-50" 						data-speed="500" 						data-start="1000" 						data-customin="x:0;y:0;z:0;rotationX:0.5;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" 						data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" 						data-easing="Back.easeOut" 						data-splitin="none" 						data-splitout="none" 						data-elementdelay="0.1" 						data-endelementdelay="0.1" 						data-endspeed="600">						<h3 class="c-block-bordered c-font-35 c-font-bold c-font-center c-font-uppercase c-font-white c-block">						<br/><br/>Investimento diferenciado  <br/ style="margin-bottom: -10px;">para investidores diferenciados</h3>					</div>					<div class="caption lft tp-resizeme" data-x="center" data-y="center" data-voffset="110" data-speed="900" data-start="2000" data-easing="easeOutExpo">						<a href="preco_custo.php" class="c-action-btn btn btn-lg c-btn-square c-theme-btn c-btn-bold c-btn-uppercase">conheça mais</a>					</div>				</li>				<!--fim -->				
+				<!--fim -->								<!--fim --><!--BEGIN: SLIDE #2 -->				<li data-transition="fade" data-slotamount="1" data-masterspeed="1000">					<img alt="" src="assets/base/img/layout/slides/pessoas2.jpg" 						data-bgposition="center center" 						data-bgfit="cover" 						data-bgrepeat="no-repeat">					<div class="caption customin customout tp-resizeme" 						data-x="center" 						data-y="center" 						data-hoffset="" 						data-voffset="-50" 						data-speed="500" 						data-start="1000" 						data-customin="x:0;y:0;z:0;rotationX:0.5;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" 						data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" 						data-easing="Back.easeOut" 						data-splitin="none" 						data-splitout="none" 						data-elementdelay="0.1" 						data-endelementdelay="0.1" 						data-endspeed="600">						<h3 class="c-block-bordered c-font-35 c-font-bold c-font-center c-font-uppercase c-font-white c-block">						<br/><br/>Investimento diferenciado para <br/ style="margin-bottom: -18px;">pessoas diferenciadas</h3>					</div>					<div class="caption lft tp-resizeme" data-x="center" data-y="center" data-voffset="110" data-speed="900" data-start="2000" data-easing="easeOutExpo">						<a href="preco_custo.php" class="c-action-btn btn btn-lg c-btn-square c-theme-btn c-btn-bold c-btn-uppercase">conheça mais</a>					</div>				</li>				<!--fim -->				
 			
 							
 				<!--BEGIN: SLIDE #4 -->
@@ -116,7 +107,7 @@ Follow: www.twitter.com/themehats
 							<div class="embed-responsive embed-responsive-16by9">								<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/en4un1xrtFU" frameborder="0" allowfullscreen>								</iframe>							</div>
 						</div>
 					</div>										
-					<div class="col-md-5">
+					<div class="col-md-5" style="margin-top: 45px;">
 						<div class="c-content-accordion-1 c-theme">
 							<div class="panel-group" id="accordion" role="tablist">
 								<div class="panel">
@@ -128,7 +119,7 @@ Follow: www.twitter.com/themehats
 									</div>
 									<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
 										<div class="panel-body c-font-18">
-											 Somos uma empresa de construção civil focada na excelência operacional . Quatro palavras definem o nosso DNA: qualidade, responsabilidade, lealdade e dedicação.
+											 Somos uma empresa de construção civil focada na excelência operacional . Quatro palavras definem o nosso DNA: qualidade, responsabilidade, lealdade e dedicação
 										</div>
 									</div>
 								</div>
@@ -142,35 +133,22 @@ Follow: www.twitter.com/themehats
 									</div>
 									<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 										<div class="panel-body c-font-18">
-											 Na construção dos empreendimentos, nas práticas de gestão, nas relações comerciais, na comunicação. A QUALIDADE está presente em todos os processos da empresa										</div>
+											 Na construção dos empreendimentos, nas práticas de gestão, nas relações comerciais, na comunicação. A QUALIDADE está presente em todos os processos da empresa.																					 </div>
 									</div>
 								</div>
-								
-								<div class="panel">
-									<div class="panel-heading" role="tab" id="headingThree">
-										<h4 class="panel-title">
-										<a class="collapsed c-font-uppercase c-font-bold c-font-19" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-										RESPONSABILIDADE</a>
-										</h4>
-									</div>
-									<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-										<div class="panel-body c-font-18">
-											 Uma prática do dia-a-dia. Nas relações com investidores, fornecedores e parceiros, bem como em relação à comunidade e ao meio ambiente.
-										</div>
-									</div>
-								</div>								
+																
 								<div class="panel">
 									<div class="panel-heading" role="tab" id="headingTwo">
 										<h4 class="panel-title">
 										<a class="collapsed c-font-uppercase c-font-bold c-font-19" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-										LEALDADE</a>
+										TRANSPARÊNCIA</a>
 										</h4>
 									</div>									
 									<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 										<div class="panel-body c-font-18">
-											 Presente em todas as relações, seja com o agentes do mercado ou com os clientes internos e externos.										</div>
+											 Presente em todas as relações da empresa: com os agentes do mercado, com os investidores e com os nossos colaboradores, a TRANSPARÊNCIA é uma das nossas diretrizes.											 </div>
 									</div>
-								</div>																								<div class="panel">									<div class="panel-heading" role="tab" id="headingThree">										<h4 class="panel-title">										<a class="collapsed c-font-uppercase c-font-bold c-font-19" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">										DEDICAÇÃO</a>										</h4>									</div>									<div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">										<div class="panel-body c-font-18">											 A empresa dedica-se aos empreendimentos e aos colaboradores que os fazem acontecer.  Nossa DEDICAÇÃO reforça os pilares da QUALIDADE, RESPONSABILIDADE e LEALDADE.										</div>									</div>								</div>								
+								</div>																								<div class="panel">									<div class="panel-heading" role="tab" id="headingThree">										<h4 class="panel-title">										<a class="collapsed c-font-uppercase c-font-bold c-font-19" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">										responsabilidade</a>										</h4>									</div>									<div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">										<div class="panel-body c-font-18">											 Nas relações com investidores,  visando um maior retorno financeiro aos seus investimentos,  como também nas relações com os fornecedores e parceiros e com a comunidade e o meio ambiente.										</div>									</div>								</div>								
 								
 							</div>
 						</div>
@@ -188,7 +166,7 @@ Follow: www.twitter.com/themehats
 		<div class="container">
 			<div class="c-content-bar-1 c-opt-1">
 				
-				<a href="contato.php" class="btn btn-md c-btn-square c-theme-btn c-btn-uppercase c-btn-Sbold">FALE AGORA COM A GENTE!</a>
+				<a href="contato.php" class="btn btn-md c-btn-square c-theme-btn c-btn-uppercase c-btn-Sbold">ENTRE EM CONTATO!</a>
 			</div>
 		</div>
 	</div>
